370edfed7d048a4c85d8874290bad24c49175889
Vijay Guttula <vijayguttula001@gmail.com>
2025-02-20 21:19:19 +0530

    Images

--- src/components/NonTechnical.js
+++ src/components/NonTechnical.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Technical.css'; // Import CSS for Technical page
+import "./Technical.css";
 
-function Technical() {
+const NonTechnical = () => {
   return (
-    <div className="technical">
-      <h1 className="technical-title">Technical Events</h1>
-      <p className="technical-description">
-        Explore and participate in a variety of non-technical events that challenge your skills and creativity.
-      </p>
+    <div className="technical-container">
+      {/* Animated Text */}
+      <h2 className="animated-text">Welcome to Non - Technical Events</h2>
 
-      <div className="technical-buttons">
-        <Link to="/redlight" className="technical-btn">Red Light, Green Light</Link>
-        <Link to="/whisperchallenge" className="technical-btn">Whisper Challenge</Link>
+      <div className="card__collection">
+        {/* Card One - Tech Torque */}
+        <div className="card-wrapper">
+          <Link to="/techtorque" className="cards cards--one">
+            <img
+              src="https://lh4.googleusercontent.com/iIqGc5X3UgyDfgHm-CTV0xS3WwVeK3XzU3lxZTq0MBQWUjRxjOb5WDgSnmgjuFZaZZIOuBdnD_WI1lkKIZ03zWhQW7uNgmL81CllmpIRwkUlJjcqxjXP2BgfCK6WJDohAA=w1280"
+              alt="Tech Torque"
+              className="card-img"
+            />
+          </Link>
+          <span className="card-name">Red Light, Green Light</span>
+        </div>
 
+        {/* Card Two - Tech Debate */}
+        <div className="card-wrapper">
+          <Link to="/techdebate" className="cards cards--two">
+            <img
+              src="https://lh3.googleusercontent.com/SpUN4wMeTLgwvYmJhKn2MBvMr8-oAahQuEi0RjXELvgTS21Rp91zN48tAiDnVcKN3RWGT-Kl1OPuPvqVSm4Rc8RMUpicK6A8OAoTdICT14yAcvodoSCXQBK9FRqfMkzTxA=w1280"
+              alt="Tech Debate"
+              className="card-img"
+            />
+          </Link>
+          <span className="card-name">Whisper Challenge</span>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Technical;
+export default NonTechnical;
